96889045f4b0c0c019659a0646479bc806b7d062
tjr2020 <tjr2020@F1093-21-2280>
2020-01-23 15:25:12 -0600

    Add project files.

---
+++ alarm/AlarmInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alarm
+{
+    public partial class AlarmInfo : Form
+    {
+        public AlarmInfo()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Set Button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SelectedTime_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ alarm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            AlarmInfo alarmEditor = new AlarmInfo();
+            alarmEditor.Show();
+        }
+    }
+}
